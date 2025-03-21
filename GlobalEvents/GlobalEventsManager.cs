1eac84e3e2031b7d723369a064ec630d624a6d37
Joshua <joshua.b.haack@gmail.com>
2025-03-21 17:43:07 +0100

    added GlobalEvents

---
+++ GlobalEvents/GlobalEventsManager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using Sirenix.Serialization;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace JohaToolkit.unity.GlobalEvents
+{
+    public static class GlobalEventsManager
+    {
+        private static readonly Dictionary<Type, List<Action>> Subscribers = new();
+
+        public static void Subscribe<T>(Action action) where T : GlobalEventBase
+        {
+            Type type = typeof(T);
+            if (!Subscribers.ContainsKey(type))
+            {
+                Subscribers.Add(type, new List<Action>());
+            }
+
+            Subscribers[type].Add(action);
+        }
+
+        public static void Unsuscribe<T>(Action action) where T : GlobalEventBase
+        {
+            Type type = typeof(T);
+            if (!Subscribers.ContainsKey(type))
+            {
+                return;
+            }
+
+            Subscribers[type].Remove(action);
+        }
+
+        public static void Raise<T>() where T : GlobalEventBase
+        {
+            Type type = typeof(T);
+            if (!Subscribers.ContainsKey(type))
+            {
+                Debug.LogWarning($"Event {type.Name} was raised, but doesnt exist!");
+                return;
+            }
+
+            foreach (Action subscriber in Subscribers[type])
+            {
+                subscriber.Invoke();
+            }
+        }
+
+        public abstract class GlobalEventBase
+        {
+            
+        }
+    }
+}
